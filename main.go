9087dba0b4b3d9693bb01e08560423a1e50bd210
snow4rider <matthew.t.mckenney@gmail.com>
2023-11-23 23:48:34 -0700

    added 404 page

--- main.go
+++ main.go
@@ -14,9 +14,25 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:matthew.t.mckenney@gmail.com\">email</a>")
 }
 
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf=8")
+	fmt.Fprint(w, "<h1>401</h1><p>page not found</p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		errorHandler(w, r)
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
