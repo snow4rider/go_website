74983cb2b2a0dbfa894ea2c39fab5284b251df38
snow4rider <matthew.t.mckenney@gmail.com>
2023-11-24 23:27:52 -0700

    added a faq page

--- main.go
+++ main.go
@@ -15,6 +15,27 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:matthew.t.mckenney@gmail.com\">email</a>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w,
+		`<h1>FAQ Page</h1>
+		<ul>
+			<li>
+				<p>Q: Is there a free version?</p>
+				<p>A: Yes! We offer a free trial for 30 days on any paid plan.</p>
+			</li>
+			<li>
+				<p>Q: What are your support hours?</p>
+				<p>A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</p>
+			</li>
+			<li>
+				<p>Q: How do I contact support?</p>
+				<p>A: Email us!</p>
+			</li>
+		</ul>
+		`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -34,6 +55,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
